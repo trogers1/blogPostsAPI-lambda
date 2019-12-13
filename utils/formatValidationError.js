b724bb3e99e8b925023544f9962179a5aeae96a0
Taylor Rogers <taylor.rogers71@gmail.com>
2019-12-13 10:56:38 -0800

    Adjust formatValidationError to use ajv

--- utils/formatValidationError.js
+++ utils/formatValidationError.js
@@ -1,14 +1,15 @@
 /**
  * 400 Validation Error, Error Handler. Can be given an already-formatted error,
- * or a jsonschema.validator error object to be formatted correctly for our
- * response.
- * @param {object}  error A jsonschema.validator error, OR an already-formatted error
+ * or a ajv.errors error array to be formatted correctly for our response.
+ *
+ * @param {Array}  error A ajv.errors array, OR an already-formatted error
  * @returns A formatted error response
  */
 module.exports.formatValidationError = error => {
   if (error.statusCode && error.headers && error.body && JSON.parse(error.body).errors) {
     return error;
   }
+  console.error('Validation Errors', error);
   return {
     statusCode: 400,
     headers: {
@@ -16,17 +17,13 @@ module.exports.formatValidationError = error => {
     },
     body: JSON.stringify({
       errors: error.map(err => {
-        let detail;
-        if (err.name === 'additionalProperties') {
-          detail = `'${err.argument}' is an invalid attribute.`;
-        } else {
-          let errorMessage = err.stack.replace('instance.', '');
-          detail = `Attribute ${errorMessage.replace(/"/g, "'")}`;
+        let detail = `${err.dataPath.replace(/\./g, '->')} ${err.message}.`;
+        if (err.keyword === 'additionalProperties') {
+          detail = `'${err.params.additionalProperty}' is an invalid attribute.`;
         }
-        console.error(`Validation error: ${detail}`);
         return {
           status: 400,
-          title: 'Invalid Attribute',
+          title: 'Bad Request',
           detail
         };
       })
